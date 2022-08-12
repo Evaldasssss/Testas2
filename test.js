8c595c79d980b2edaa3cb3bb87713e0ccded3447
Evaldas <m.evaldas@yahoo.com>
2022-08-12 10:55:18 +0300

    7 week final task submit

---
+++ test.js
@@ -0,0 +1,48 @@
+// task - 1
+
+const arr = [1, 3];
+
+console.log(Math.max(...arr));
+
+console.log('===========')
+
+// task - 2
+
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+console.log('===========')
+
+// task - 3
+
+for (let i = 0; i < 11; i += 2) {
+    console.log(i);
+}
+
+console.log('===========')
+
+// task - 4
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(rand(1, 10))
+console.log(rand(1, 10))
+console.log(rand(1, 10))
+console.log(rand(1, 10))
+console.log(rand(1, 10))
+console.log('===========')
+
+// task - 5
+
+// task - 6
+// task - 7
+
+
+// task - 8
+// task - 9
+// task - 10
